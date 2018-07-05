0f8f22834351ab6257aff9b6a221336e964f5eae
JudyVue <jvue@blulinksolutions.com>
2018-07-05 11:45:13 -0700

    adding day 19 load testing

---
+++ back-end/19-load-testing/load-testing/load-test-create-user.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const faker = require('faker');
+const uuid = require('uuid/v4');
+const loadTestUser  = module.exports = {};
+
+loadTestUser.create = (userContext, events, done) => {
+  // properties from my account schema
+  userContext.vars.username = faker.internet.userName() + uuid();
+  userContext.vars.email = faker.internet.email() + uuid();
+  userContext.vars.password = faker.internet.password() + uuid();
+
+  // properties from my profile schema
+  userContext.vars.bio = faker.lorem.words(10) + uuid();
+  userContext.vars.firstName = faker.name.firstName() + uuid();
+  userContext.vars.lastName = faker.name.lastName() +  uuid();
+  return done();
+}
